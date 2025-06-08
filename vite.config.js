42a36806953a65da96e69ba3409f4ef57820c83e
Dvrklex <dvrklex@gmail.com>
2025-06-08 11:00:39 -0300

    FIX> deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [vue()]
+  base: 'dvrklex.github.io',
+  plugins: [vue(), ghPages()]
 })
